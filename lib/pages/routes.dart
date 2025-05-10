6cca78aa5d47fe879c8e355e55bb3e8181f9a9e0
willy Chou <willy931118@gmail.com>
2025-05-10 18:28:15 +0800

    add temporary home page route

--- lib/pages/routes.dart
+++ lib/pages/routes.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:invenza/pages/home_page.dart';
 import 'package:invenza/pages/login_page.dart';
 
 final Map<String, WidgetBuilder> routes = {
-  '/' : (context) => const LoginPage()
+  '/' : (context) => const LoginPage(),
+  '/home' : (context) => const HomePage(),
 };
