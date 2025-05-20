e6631bc7549cd382a7662932b66174085713a891
willy Chou <willy931118@gmail.com>
2025-05-20 19:06:04 +0800

    set homepage for changing page with app bar in tablet or pc

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:invenza/theme/theme.dart';
 
 void main() {
   final colorScheme = ColorScheme.fromSeed(seedColor: Color(0xFF51B5FF));
-  printColorScheme(colorScheme);
   runApp(
       ProviderScope(
         child: Invenza(),
