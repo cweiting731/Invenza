22b30d460f15e69cfd9920cc0ac394a6a4873458
willy Chou <willy931118@gmail.com>
2025-05-21 13:39:22 +0800

    add api_route

---
+++ lib/providers/api_route.dart
@@ -0,0 +1,12 @@
+
+class ApiRoute {
+  // static const _serverAddress = 'http://192.168.215.198:8080';
+  static const _serverAddress = 'http://localhost:8080';
+  static const Map<String, String> _apiRoute = {
+    'auth' : '/api/auth/login',
+    'forgot-password' : '/api/auth/forgot-password',
+    'issue-report' : '/api/issue-report',
+  };
+
+  static String getRoute(String type) => '$_serverAddress${_apiRoute[type]}';
+}
