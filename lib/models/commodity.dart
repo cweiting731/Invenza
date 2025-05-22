8b811012cfc58836f156b69fedc9914d57b8b48c
willy Chou <willy931118@gmail.com>
2025-05-22 14:56:50 +0800

    create ProcurementPage's item model

---
+++ lib/models/commodity.dart
@@ -0,0 +1,8 @@
+import 'package:invenza/models/transaction_value.dart';
+
+class Commodity {
+  String name;
+  String type;
+  TransactionValue transactionValue;
+  Commodity(this.name, this.type, this.transactionValue);
+}
---
+++ lib/providers/procurement_provider.dart
@@ -0,0 +1,10 @@
+
+
+
+import 'package:invenza/models/import_order.dart';
+
+class ProcurementRepository {
+  // Future<List<ImportOrder>> fetchOrders() async {
+  //
+  // }
+}
