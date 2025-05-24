4cb57e0a96f722d95e48d7021d5a0576f92b71e5
willy Chou <willy931118@gmail.com>
2025-05-24 17:32:00 +0800

    implement fetchOrders and show it on the page

--- lib/pages/home_page/procurement_page.dart
+++ lib/pages/home_page/procurement_page.dart
@@ -5,8 +5,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:invenza/models/association.dart';
 import 'package:invenza/models/business_partner.dart';
 import 'package:invenza/models/commodity.dart';
+import 'package:invenza/models/employee.dart';
 import 'package:invenza/models/import_order.dart';
 import 'package:invenza/models/transaction_value.dart';
+import 'package:invenza/models/transfer_data/filter_options.dart';
+import 'package:invenza/providers/api_provider.dart';
+
+import '../../providers/procurement_provider.dart';
 
 class ProcurementPage extends ConsumerStatefulWidget {
   const ProcurementPage({super.key});
@@ -15,50 +20,59 @@ class ProcurementPage extends ConsumerStatefulWidget {
 }
 
 class _ProcurementPageState extends ConsumerState<ProcurementPage> {
+  late FilterOptions _filters;
 
   @override
   void initState() {
-    // TODO: implement initState
+    _filters = FilterOptions();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: fakeData.length,
-      itemBuilder: (context, index) {
-        final order = fakeData[index];
-        return Card(
-          surfaceTintColor: Colors.transparent,
-          shadowColor: Colors.black12,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-          margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-          child: ExpansionTile(
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-            collapsedShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-            title: Text('${order.id ?? '?'}'),
-            subtitle: Text('${order.commodity?.name ?? '查無商品名稱'} / ${order.commodity?.type ?? '查無商品型號'}'),
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(12),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Text('單價: ${order.commodity?.transactionValue.unitPrice ?? '查無商品單價'}'),
-                    Text('數量: ${order.commodity?.transactionValue.quantity ?? '查無商品數量'}'),
-                    Text('總價: ${order.commodity?.transactionValue.totalCost ?? '查無商品總價'}'),
-                    Text('供應商: ${order.supplier?.name ?? '查無供應商名稱'}'),
-                    Text('供應商email: ${order.supplier?.association.email ?? '查無供應商email'}'),
-                    Text('供應商電話: ${order.supplier?.association.phone ?? '查無供應商電話'}'),
-                    Text('下單日期: ${order.orderTimeStamp ?? '查無下單日期'}'),
-                    Text('截止日期: ${order.deadlineTimeStamp ?? '查無截止日期'}'),
-                    Text('負責人: ${order.responsible}'),
-                  ],
-                ),
-              )
-            ],
-          ),
+    final asyncOrders = ref.watch(importOrdersProvider(_filters));
+    final api = ref.read(apiClientProvider);
+    return asyncOrders.when(
+      loading: () => Center(child: CircularProgressIndicator(),),
+      error: (err, _) => Center(child: Text(api.formatErrorMessage(err)),),
+      data: (orders) {
+        return ListView.builder(
+          itemCount: orders.length,
+          itemBuilder: (context, index) {
+            final order = orders[index];
+            return Card(
+              surfaceTintColor: Colors.transparent,
+              shadowColor: Colors.black12,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+              margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+              child: ExpansionTile(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                collapsedShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                title: Text('${order.id ?? '?'}'),
+                subtitle: Text('${order.commodity?.name ?? '查無商品名稱'} / ${order.commodity?.type ?? '查無商品型號'}'),
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Text('單價: ${order.commodity?.transactionValue.unitPrice ?? '查無商品單價'}'),
+                        Text('數量: ${order.commodity?.transactionValue.quantity ?? '查無商品數量'}'),
+                        Text('總價: ${order.commodity?.transactionValue.totalCost ?? '查無商品總價'}'),
+                        Text('供應商: ${order.supplier?.name ?? '查無供應商名稱'}'),
+                        Text('供應商email: ${order.supplier?.association.email ?? '查無供應商email'}'),
+                        Text('供應商電話: ${order.supplier?.association.phone ?? '查無供應商電話'}'),
+                        Text('下單日期: ${order.orderTimeStamp ?? '查無下單日期'}'),
+                        Text('截止日期: ${order.deadlineTimeStamp ?? '查無截止日期'}'),
+                        Text('負責人: ${order.responsible}'),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
         );
       },
     );
--- lib/providers/procurement_provider.dart
+++ lib/providers/procurement_provider.dart
@@ -1,10 +1,47 @@
-
-
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:invenza/models/employee.dart';
 import 'package:invenza/models/import_order.dart';
+import 'package:invenza/models/transfer_data/filter_options.dart';
+import 'package:invenza/providers/api_provider.dart';
+import 'package:invenza/providers/api_route.dart';
+import 'package:invenza/providers/auth_provider.dart';
+import 'package:invenza/services/api_client.dart';
+
+final importOrdersProvider = FutureProvider.family<List<ImportOrder>, FilterOptions>((ref, filters) async {
+  final repo = ref.read(procurementRepositoryProvider);
+  return repo.fetchOrders(filters: filters);
+});
+
+final procurementRepositoryProvider = Provider<ProcurementRepository>((ref) {
+  final api = ref.read(apiClientProvider);
+  final user = ref.read(userProvider);
+  return ProcurementRepository(api, user);
+});
 
 class ProcurementRepository {
-  // Future<List<ImportOrder>> fetchOrders() async {
-  //
-  // }
+  final ApiClient _api;
+  final Employee? _user;
+
+  ProcurementRepository(this._api, this._user);
+
+  Future<List<ImportOrder>> fetchOrders({FilterOptions? filters}) async {
+    final queryParams = filters?.toQueryParams();
+
+    try {
+      if (_user == null || _user.jwtToken == null) {
+        throw Exception('尚未登入，你怎麼進來的?');
+      }
+      final data = await _api.get(
+        ApiRoute.getRoute('procurement-get-data'),
+        queryParams: queryParams,
+        token: _user.jwtToken!
+      );
+
+      final List rawData = data['data'];
+      return rawData.map( (order) => ImportOrder.fromJson(order)).toList();
+    } catch (e, st) {
+      print(e);
+      rethrow;
+    }
+  }
 }
