818e1e0f05d00d4c1f22456601b018efeee40d22
willy Chou <willy931118@gmail.com>
2025-06-14 16:08:33 +0800

    add http.delete function

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -107,6 +107,36 @@ class ApiClient {
     }
   }
 
+  Future<Map<String, dynamic>> delete(String url, Map<String, String>? queryParams,{String token = ''}) async {
+    try {
+      final response = await http.delete(
+        Uri.parse(url).replace(queryParameters: queryParams),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      log.i('Response headers: ${response.headers}');
+      log.i('Response body (pretty):\n${ JsonEncoder.withIndent('  ').convert(jsonDecode(utf8.decode(response.bodyBytes))) }');
+      final data = jsonDecode(utf8.decode(response.bodyBytes));
+
+      if (response.statusCode == 200) {
+        return data;
+      } else {
+        throw Exception(data['error'] ?? '伺服器錯誤 (${response.statusCode})');
+      }
+    } on SocketException catch (e, st) {
+      Error.throwWithStackTrace(Exception('無法連接伺服器，請檢查網路連線'), st);
+    } on FormatException catch (e, st) {
+      Error.throwWithStackTrace(Exception('資料格式錯誤，請聯繫開發人員'), st);
+    } on http.ClientException catch (e, st) {
+      Error.throwWithStackTrace(Exception('連線失敗，請確認伺服器是否有開啟'), st);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
   String formatErrorMessage(Object? error) {
     if (error is Exception) {
       return error.toString().replaceFirst('Exception: ', '');
