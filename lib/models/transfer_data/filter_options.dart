9a703a9354fc924176654bdd3c3fb2bcabb46464
willy Chou <willy931118@gmail.com>
2025-06-15 11:46:48 +0800

    fill all filterOptions

--- lib/models/transfer_data/filter_options.dart
+++ lib/models/transfer_data/filter_options.dart
@@ -1,35 +1,62 @@
+import 'package:intl/intl.dart';
+enum InventoryFilterType {
+  inventoryNow, 
+  inventoryFuture,
+}
 class FilterOptions {
   final String? commodityName;
   final String? commodityType;
+
+  /// Procurement/SalesRecords page
   final String? businessPartner;
-  final DateTime? orderTime;
-  final DateTime? deadline;
+  final String? businessPartnerId;
+  final DateTime? orderTimeStart;
+  final DateTime? orderTimeEnd;
+  final DateTime? deadlineStart;
+  final DateTime? deadlineEnd;
+
+  /// Inventory page
+  final double? minAmount;
+  final double? maxAmount;
+  final InventoryFilterType? inventoryFilterType;
+
   final String? responsible;
-  final int? startIndex;
-  final int? amount;
+  final String? responsibleId;
 
 
   FilterOptions({
     this.commodityName,
     this.commodityType,
     this.businessPartner,
-    this.orderTime,
-    this.deadline,
+    this.businessPartnerId,
+    this.orderTimeStart,
+    this.orderTimeEnd,
+    this.deadlineStart,
+    this.deadlineEnd,
+    this.minAmount,
+    this.maxAmount,
+    this.inventoryFilterType,
     this.responsible,
-    this.startIndex,
-    this.amount
+    this.responsibleId,
   });
 
   Map<String, String> toQueryParams() {
+    final format = DateFormat("yyyy-MM-dd HH:mm");
     final Map<String, String> params = {};
     if (commodityName != null) params['commodityName'] = commodityName!;
     if (commodityType != null) params['commodityType'] = commodityType!;
     if (businessPartner != null) params['businessPartner'] = businessPartner!;
-    if (orderTime != null) params['orderTime'] = orderTime!.toIso8601String();
-    if (deadline != null) params['deadline'] = deadline!.toIso8601String();
+    if (businessPartnerId != null) params['businessPartnerId'] = businessPartnerId!;
+    if (orderTimeStart != null) params['orderTimeStart'] = format.format(orderTimeStart!);
+    if (orderTimeEnd != null) params['orderTimeEnd'] = format.format(orderTimeEnd!);
+    if (deadlineStart != null) params['deadlineStart'] = format.format(deadlineStart!);
+    if (deadlineEnd != null) params['deadlineEnd'] = format.format(deadlineEnd!);
+    if (minAmount != null) params['minAmount'] = minAmount!.toString();
+    if (maxAmount != null) params['maxAmount'] = maxAmount!.toString();
+    if (inventoryFilterType != null) params['inventoryFilterType'] = inventoryFilterType!.toString().split('.').last;
     if (responsible != null) params['responsible'] = responsible!;
-    if (startIndex != null) params['startIndex'] = startIndex!.toString();
-    if (startIndex != null) params['amount'] = amount!.toString();
+    if (responsibleId != null) params['responsibleId'] = responsibleId!;
+    // Ensure all values are non-null and converted to strings
     return params;
   }
 
