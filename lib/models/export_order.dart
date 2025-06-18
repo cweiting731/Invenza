3ae4c8a45806907fcda7e1593539661b3a135a82
willy Chou <willy931118@gmail.com>
2025-06-18 17:32:10 +0800

    add export_order

---
+++ lib/models/export_order.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+
+import 'package:intl/intl.dart';
+import 'package:invenza/interface/serializable.dart';
+import 'package:invenza/models/business_partner.dart';
+import 'package:invenza/models/commodity.dart';
+import 'package:invenza/models/employee.dart';
+
+class Exportorder implements Serializable {
+  int? id;
+  Commodity? commodity;
+  BusinessPartner? distributor;
+  DateTime? orderTimeStamp;
+  DateTime? deadlineTimeStamp;
+  Employee? responsible;
+
+  Exportorder({
+    this.id,
+    this.commodity,
+    this.distributor,
+    this.orderTimeStamp,
+    this.deadlineTimeStamp,
+    this.responsible,
+  });
+
+  factory Exportorder.fromJson(Map<String, dynamic> json) {
+    final commodity = json['commodity'];
+    final distributor = json['distributor'];
+    final responsible = json['responsible'];
+
+    DateTime? orderTimeStamp;
+    DateTime? deadlineTimeStamp;
+
+    try {
+      orderTimeStamp = DateTime.parse(json['orderTimeStamp']);
+    } catch (_) {
+      orderTimeStamp = null;
+    }
+
+    try {
+      deadlineTimeStamp = DateTime.parse(json['deadlineTimeStamp']);
+    } catch (_) {
+      deadlineTimeStamp = null;
+    }
+
+    return Exportorder(
+      id: json['id'],
+      commodity: Commodity.fromJson(commodity),
+      distributor: BusinessPartner.fromJson(distributor),
+      orderTimeStamp: orderTimeStamp,
+      deadlineTimeStamp: deadlineTimeStamp,
+      responsible: Employee.fromJson(responsible),
+    );
+  }
+
+  @override
+  String serialization() {
+    final formatter = DateFormat("yyyy-MM-dd HH:mm");
+    return jsonEncode({
+      "id": id,
+      "commodity": commodity?.toJson(),
+      "distributor": distributor?.toJson(),
+      "orderTimeStamp": orderTimeStamp != null ? formatter.format(orderTimeStamp!) : null,
+      "deadlineTimeStamp": deadlineTimeStamp != null ? formatter.format(deadlineTimeStamp!) : null,
+      "responsible": responsible?.toJson(),
+    });
+  }
+}
