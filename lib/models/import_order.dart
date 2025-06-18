c4d445e6673b0e2c15f796cecc37543b0fccfb2d
willy Chou <willy931118@gmail.com>
2025-06-18 17:55:11 +0800

    remove unused import

--- lib/models/import_order.dart
+++ lib/models/import_order.dart
@@ -2,11 +2,9 @@ import 'dart:convert';
 
 import 'package:intl/intl.dart';
 import 'package:invenza/interface/serializable.dart';
-import 'package:invenza/models/association.dart';
 import 'package:invenza/models/business_partner.dart';
 import 'package:invenza/models/commodity.dart';
 import 'package:invenza/models/employee.dart';
-import 'package:invenza/models/transaction_value.dart';
 
 class ImportOrder implements Serializable{
   // 進貨單ID(系統自動給)、商品名稱、型號、供應商名稱、供應商編號、供應商聯絡方式、進貨單價、進貨數量、進貨總價、訂單日期、進貨日期、填單負責人(根據login資料自動填入)
